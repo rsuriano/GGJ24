064f2f4bd1728848f08d0c195ffcd121cc60060d
Ramiro <ramirosuriano@hotmail.com>
2024-01-28 14:02:17 -0300

    finish main menu

--- Scenes/MainScreen.gd
+++ Scenes/MainScreen.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
-# Called when the node enters the scene tree for the first time.
+
 func _ready():
 	$StartButton/Start_Boton/StartBoton_anim.playback_speed = 10
+	$CreditsButton/credits_boton/creditsboton_anim.playback_speed = 10
 
+# START BUTTON
 func _on_Button_button_up():
-	
 	$StartButton/Start_Boton/StartBoton_anim.play("StartBoton_anim")
 
 func _on_StartBoton_anim_animation_finished(anim_name):
 	GlobalSceneManager.goto_scene("res://Scenes/MainGame.tscn")
 
 
+# CREDITS BUTTON
 func _on_CreditsButton_button_up():
+	$CreditsButton/credits_boton/creditsboton_anim.play("CreditsBoton_anim")
+
+func _on_creditsboton_anim_animation_finished(anim_name):
 	GlobalSceneManager.goto_scene("res://Scenes/Credits.tscn")
